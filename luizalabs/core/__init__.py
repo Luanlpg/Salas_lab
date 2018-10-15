03a1c7d215af5d6cd19a86daaf454c20df2c8b90
Luan Santos <luanlpg13@gmail.com>
2018-10-15 19:17:24 -0300

    setup

---
+++ luizalabs/core/__init__.py
@@ -0,0 +1,33 @@
+"""
+Coding Test
+O Problema
+
+O LuizaLabs está em constante crescimento e precisamos de um sistema que faça a gestão das salas de
+reuniões do nosso escritório.
+Assim como todos as aplicações criadas na LuizaLabs, ela precisa ter uma API para permitir integrações com
+outros sistemas.
+Esse sistema deve receber requisições de agendamento contendo título, sala e período de agendamento e
+deve apenas reservar a sala, se a sala requisitada estiver disponível. Caso contrário, deve apresentar um
+erro.
+
+Recomendações
+● Crie essa aplicação como se fosse uma aplicação real, que seria utilizada pelo Magazine Luiza
+● Escreva testes
+● Utilize o design pattern SOLID
+● Utilize boas práticas de programação
+● Utilize boas práticas de git
+● Modelo o banco de dados da melhor forma
+Requisitos
+● O Webservice deve seguir os princípios REST
+● Salvar as informações necessárias em um banco de dados (relacional ou não), de sua escolha
+● Testes automatizados com informação da cobertura de testes
+● Gerar logs das ações
+● Documentar como rodar o projeto
+Entregáveis
+O sistema de agendamento precisa ter:
+● Uma API para listar e filtrar agendamentos por data e sala
+● Uma API para criar, editar e remover agendamentos
+● Uma API para criar, editar e remover salas de reuniões
+
+Qualquer dúvida sobre o teste, fique a vontade para entrar em contato conosco.
+"""
